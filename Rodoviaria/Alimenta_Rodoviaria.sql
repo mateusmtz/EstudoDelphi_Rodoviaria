d12ca3ade7d098efe594a286d82a0d8818042f75
mateusmtz <88405324+mateusmtz@users.noreply.github.com>
2023-06-03 01:52:49 -0300

    Script e Banco

---
+++ Rodoviaria/Alimenta_Rodoviaria.sql
@@ -0,0 +1,28 @@
+INSERT INTO Empresas VALUES (101, 'FLORES');
+INSERT INTO Empresas VALUES (102, 'REGINAS');
+INSERT INTO Empresas VALUES (103, 'MASTER');
+INSERT INTO Empresas VALUES (104, 'RUBANIL');
+INSERT INTO Empresas VALUES (105, 'REAL');
+
+INSERT INTO Motoristas VALUES (341,'Paulo Roberto' ,35 , 'M' ,1200);
+INSERT INTO Motoristas VALUES (342,'Anderson Moraes' ,24 , 'M' ,1150);
+INSERT INTO Motoristas VALUES (343,'Fernando Santana' ,28 , 'M' ,1170);
+INSERT INTO Motoristas VALUES (344,'Carlos Paiva' ,40 , 'M' ,1320);
+INSERT INTO Motoristas VALUES (345,'Alberto Araújo' ,25 , 'M' ,890);
+INSERT INTO Motoristas VALUES (346,'Jorge Luiz' ,43 , 'M' ,1400);
+INSERT INTO Motoristas VALUES (347,'Maria Padinha' ,29 , 'M' ,900);
+INSERT INTO Motoristas VALUES (348,'Claudia da Silva' ,32 , 'F' ,1000);
+INSERT INTO Motoristas VALUES (349,'Jeferson Gomes' ,30 , 'M' ,1380);
+INSERT INTO Motoristas VALUES (350,'Felipe Castro' ,19 , 'M' ,650);
+
+
+INSERT INTO Onibus VALUES (2451,101 ,341 , 'Nova Iguaçu x Caxias');
+INSERT INTO Onibus VALUES (2452,101 ,342 , 'São João x Praça XV');
+INSERT INTO Onibus VALUES (2453,101 ,343 , 'Mesquita x Praça Mauá');
+INSERT INTO Onibus VALUES (4213,102 ,344 , 'Pavuna x Castelo');
+INSERT INTO Onibus VALUES (4214,102 ,345 , 'Nova Iguaçu X Pavuna');
+INSERT INTO Onibus VALUES (4215,102 ,346 , 'Caxias X São João');
+INSERT INTO Onibus VALUES (4216,102 ,347 , 'Nilópolis X Pavuna');
+INSERT INTO Onibus VALUES (5987,103 ,348 , 'Queimados x Pavuna');
+INSERT INTO Onibus VALUES (5988,103 ,349 , 'Campo Grande X Caxias');
+INSERT INTO Onibus VALUES (1702,104 ,350 , 'São João x Itaguaí');
